b5ce1221a0d511603e30fd08b55c0256dc319f8d
AlexPapas <alex-papas@hotmail.com>
2021-01-17 15:36:12 +0100

    update

---
+++ db.py
@@ -0,0 +1,29 @@
+create_show = Venue(
+    id=1,
+    name="The Musical Hop",
+    city="San Francisco",
+    state="CA",
+    address="1015 Folsom Street",
+    genres=["Jazz", "Reggae", "Swing", "Classical", "Folk"],
+    phone="123-123-1234",
+    image_link="https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+    facebook_link="https://www.facebook.com/TheMusicalHop",
+    website="https://www.themusicalhop.com",
+    seeking_talent=True,
+    seeking_description="We are on the lookout for a local artist to play every two weeks. Please call us.",
+)
+
+
+create_show = Venue(
+    id=3,
+    name="Park Square Live Music & Coffee",
+    genres=["Classical", "R&B", "Hip-Hop"],
+    address="34 Whiskey Moore Ave",
+    city="San Francisco",
+    state="CA",
+    phone="415-000-1234",
+    website="https://www.parksquarelivemusicandcoffee.com",
+    facebook_link="https://www.facebook.com/ParkSquareLiveMusicAndCoffee",
+    seeking_talent=False,
+    image_link="https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
+)
